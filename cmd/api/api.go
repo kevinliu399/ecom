9ac37de9e5182a207dcade65da7e041d5b17758e
Kevin Liu <kevin.liu4@mail.mcgill.ca>
2024-07-21 23:59:31 -0400

    fiorst

---
+++ cmd/api/api.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kevinliu399/ecom/service/user"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter() // /api/v1 for RESTful principles
+
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(subrouter) // /api/v1/login, /api/v1/register, prefixed
+
+	log.Println("listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
---
+++ types/types.go
@@ -0,0 +1,24 @@
+package types
+
+type RegisterUserPayload struct {
+	FirstName string `json:"first_name" validate:"required"`
+	LastName  string `json:"last_name"  validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=3,max=130"`
+}
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at"`
+}
+
+// we use interface so it is more easily testable
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
